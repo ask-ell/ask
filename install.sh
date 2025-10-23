69a212f2c6d26f046ef647fd8e49b9496703b8b6
Yannick Debrée <ydebree@peaks.fr>
2025-10-24 01:23:58 +0200

    Install bash file created

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+curl -o ask https://raw.githubusercontent.com/ask-ell/ask/refs/heads/release/bin
+
+BINARY_FILE_PATH=/usr/local/bin/ask
+
+sudo mv ask "$BINARY_FILE_PATH"
+
+sudo chown $USER:$USER "$BINARY_FILE_PATH"
+
+chmod +x "$BINARY_FILE_PATH"
