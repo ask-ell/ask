d8e41d9b6051aaee17e43108ebedbf73eb3d970a
Yannick Debree <askellcompany@gmail.com>
2026-04-28 19:18:01 +1200

    back-end requirements installed

--- services/back-end/src/infra/nest/index.ts
+++ services/back-end/src/infra/nest/index.ts
@@ -1,7 +1,19 @@
 import { NestFactory } from "@nestjs/core";
+import { INestApplication } from "@nestjs/common";
 import { IServerProvider } from "@ask-ell/core";
+import { AddOnWrapper } from "@ask-ell/back-end";
 
 import { AppModule } from "./app.module";
 
 
-export const createBackend = (): Promise<IServerProvider> => NestFactory.create(AppModule);
+export async function createBackend(): Promise<IServerProvider> {
+    const application: INestApplication = await NestFactory.create(AppModule);
+
+    new AddOnWrapper({
+        swagger: {
+            title: 'Ask Back-end API'
+        }
+    }).apply(application);
+
+    return application;
+};
--- services/back-end/src/infra/nest/modules/project/dto/project.dto.ts
+++ services/back-end/src/infra/nest/modules/project/dto/project.dto.ts
@@ -3,6 +3,7 @@ import { Id, IProjectDTO, ITaskDTO } from '@ask-ell/back-end-api';
 
 export class ProjectDTO implements IProjectDTO {
     id!: Id;
+    type!: 'project';
     configuration!: Id;
     tasks?: ITaskDTO[];
 }
