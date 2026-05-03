948486cfee7070ae67535c522eac620d0d94d275
Yannick Debree <askellcompany@gmail.com>
2026-05-03 18:18:49 +1200

    Options added in Ask command

--- packages/cli/src/infra/commander/commands/ask.command.ts
+++ packages/cli/src/infra/commander/commands/ask.command.ts
@@ -6,7 +6,10 @@ import { RootOptions } from "../types";
 export class AskCommand extends Command {
     constructor() {
         super('ask');
-        this.option('-s, --storage [PATH]', 'Path to the storage directory', '$HOME/.ask'); // TODO: make creating it in home directory
+        this
+            .option('-s, --storage [PATH]', 'Storage directory path', '$HOME/.ask') // TODO: make creating it in home directory
+            .option('-r, --remote [HOST]', 'Remote host')
+            .option('-f, --fixtures [PATH]', 'Fixtures file path');
     }
 
     getRootOptions(): RootOptions {
