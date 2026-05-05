0df9d74f682cd5a066713eae090250c55d6815e7
Yannick Debree <askellcompany@gmail.com>
2026-05-05 18:31:46 +1200

    SignaleLogger class improved

--- packages/cli/src/infra/signale/signale.logger.ts
+++ packages/cli/src/infra/signale/signale.logger.ts
@@ -1,21 +1,5 @@
 import { ILogger } from "@ask-ell/core";
-import signale from 'signale';
+import { Signale } from 'signale';
 
 
-export class SignaleLogger implements ILogger {
-    log(...data: any[]): void {
-        signale.log(...data);
-    }
-
-    info(...data: any[]): void {
-        signale.info(...data);
-    }
-
-    warn(...data: any[]): void {
-        signale.warn(...data);
-    }
-
-    error(...data: any[]): void {
-        signale.error(...data);
-    }
-}
+export class SignaleLogger extends Signale implements ILogger {}
