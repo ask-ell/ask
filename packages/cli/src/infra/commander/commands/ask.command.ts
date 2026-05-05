87fc2f027863165f33cf2a7c754aefe214b0471b
Yannick Debree <askellcompany@gmail.com>
2026-05-05 18:31:33 +1200

    Remote host defined as remote URL in root options

--- packages/cli/src/infra/commander/commands/ask.command.ts
+++ packages/cli/src/infra/commander/commands/ask.command.ts
@@ -1,6 +1,6 @@
 import { Command } from "commander";
 
-import { RootOptions } from "../types";
+import { RootOptions } from "../../../shared/root-options";
 
 
 export class AskCommand extends Command {
@@ -8,7 +8,7 @@ export class AskCommand extends Command {
         super('ask');
         this
             .option('-s, --storage [PATH]', 'Storage directory path', '$HOME/.ask') // TODO: make creating it in home directory
-            .option('-r, --remote [HOST]', 'Remote host')
+            .option('-r, --remote [URL]', 'Remote URL')
             .option('-l, --log [LEVEL]', 'Logger level')
             .allowExcessArguments();
     }
