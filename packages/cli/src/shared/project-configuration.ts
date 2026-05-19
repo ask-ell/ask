78bafd7af3c954ea079a8d61f7bc7fb2aa3a5ba2
Yannick Debree <askellcompany@gmail.com>
2026-05-19 21:33:11 +1200

    Format command runned

--- packages/cli/src/shared/project-configuration.ts
+++ packages/cli/src/shared/project-configuration.ts
@@ -1,66 +1,106 @@
-import { HttpClient, ILogger, IResult, MaybeUndefined, fail, success } from "@ask-ell/core";
+import {
+  HttpClient,
+  ILogger,
+  IResult,
+  MaybeUndefined,
+  fail,
+  success,
+} from '@ask-ell/core';
 
-import { IProjectConfigurationDTO, IProjectConfigurationPartialDTO, IProjectDTO } from "@ask/back-end-api";
+import {
+  IProjectConfigurationDTO,
+  IProjectConfigurationPartialDTO,
+  IProjectDTO,
+} from '@ask/back-end-api';
 
+export type ProjectConfigurationProvider<Args extends any[]> = (
+  ...args: Args
+) => Promise<IProjectConfigurationDTO[]>;
 
-export type ProjectConfigurationProvider<Args extends any[]> = (...args: Args) => Promise<IProjectConfigurationDTO[]>;
-
-export const getProjectConfigurations = ({
-    logger
-}: {
-    logger: ILogger
-}): ProjectConfigurationProvider<[IProjectDTO]> => async (project: IProjectDTO): Promise<IProjectConfigurationDTO[]> => {
+export const getProjectConfigurations =
+  ({
+    logger,
+  }: {
+    logger: ILogger;
+  }): ProjectConfigurationProvider<[IProjectDTO]> =>
+  async (project: IProjectDTO): Promise<IProjectConfigurationDTO[]> => {
     const { extends: projectConfigurationPartials } = project;
 
-    if(!projectConfigurationPartials) {
-        return Promise.resolve([]);
+    if (!projectConfigurationPartials) {
+      return Promise.resolve([]);
     }
 
     // // TODO: extract
     const defaultRemote: string = 'http://localhost:3000';
 
-    const projectConfigurationFetchingResults: IResult<IProjectConfigurationDTO>[] = await Promise.all(
+    const projectConfigurationFetchingResults: IResult<IProjectConfigurationDTO>[] =
+      await Promise.all(
         projectConfigurationPartials.map(
-            async ({ id, remote, version }: IProjectConfigurationPartialDTO): Promise<IResult<IProjectConfigurationDTO>> => {
-                // TODO: read files cache here
+          async ({
+            id,
+            remote,
+            version,
+          }: IProjectConfigurationPartialDTO): Promise<
+            IResult<IProjectConfigurationDTO>
+          > => {
+            // TODO: read files cache here
 
-                const remoteUrl: URL = new URL(remote ?? defaultRemote);
-                let path: string = `project-configurations/${id}`;
+            const remoteUrl: URL = new URL(remote ?? defaultRemote);
+            let path: string = `project-configurations/one?id=${id}`;
 
-                if(version) {
-                    path += `/${version}`;
-                }
-
-                const projectConfigurationFetchingResult: IResult<IProjectConfigurationDTO> = await HttpClient
-                    .get<{data: IProjectConfigurationDTO}>({ // TODO: add as type ?
-                        url: new URL(path, remoteUrl)
-                    })
-                    .then((response:  IResult<{data: IProjectConfigurationDTO}>): IResult<IProjectConfigurationDTO> =>{
-                        return success(response.getData()!.data);
-                    })
-                    .catch((error: any): IResult => {
-                        return fail(error);
-                    });
-
-                if(projectConfigurationFetchingResult.isAFail()) {
-                    let errorMessage: string = projectConfigurationFetchingResult.getError().message;
-
-                    const errorData: MaybeUndefined<any> = (projectConfigurationFetchingResult.getError() as any).data; // TODO: add type
-                    if(errorData?.message) {
-                        errorMessage = errorData.message;
-                    }
-
-                    logger.error(`Error for project configuration "${id}" : ${errorMessage}`);
-                }
-
-                return projectConfigurationFetchingResult;
+            if (version) {
+              path += `&version=${version}`;
             }
-        )
-    );
+
+            const projectConfigurationFetchingResult: IResult<IProjectConfigurationDTO> =
+              await HttpClient.get<{ data: IProjectConfigurationDTO }>({
+                // TODO: add as type ?
+                url: new URL(path, remoteUrl),
+              })
+                .then(
+                  (
+                    response: IResult<{ data: IProjectConfigurationDTO }>,
+                  ): IResult<IProjectConfigurationDTO> => {
+                    return success(response.getData()!.data);
+                  },
+                )
+                .catch((error: any): IResult => {
+                  return fail(error);
+                });
+
+            if (projectConfigurationFetchingResult.isAFail()) {
+              let errorMessage: string =
+                projectConfigurationFetchingResult.getError().message;
+
+              const errorData: MaybeUndefined<any> = (
+                projectConfigurationFetchingResult.getError() as any
+              ).data; // TODO: add type
+              if (errorData?.message) {
+                errorMessage = errorData.message;
+              }
+
+              logger.error(
+                `Error for project configuration "${id}" : ${errorMessage}`,
+              );
+            }
+
+            return projectConfigurationFetchingResult;
+          },
+        ),
+      );
 
     // TODO: fetch nested configurations
     // TODO: store in cache here
     return projectConfigurationFetchingResults
-        .filter((projectConfigurationFetchingResult: IResult<IProjectConfigurationDTO>): boolean => projectConfigurationFetchingResult.isASuccess())
-        .map((projectConfigurationFetchingResult: IResult<IProjectConfigurationDTO>): IProjectConfigurationDTO => projectConfigurationFetchingResult.getData()!);
-}
+      .filter(
+        (
+          projectConfigurationFetchingResult: IResult<IProjectConfigurationDTO>,
+        ): boolean => projectConfigurationFetchingResult.isASuccess(),
+      )
+      .map(
+        (
+          projectConfigurationFetchingResult: IResult<IProjectConfigurationDTO>,
+        ): IProjectConfigurationDTO =>
+          projectConfigurationFetchingResult.getData()!,
+      );
+  };
