48f8b2950d7b2e4329848e99f61a44d63a6e58c4
Yannick Debree <askellcompany@gmail.com>
2026-06-03 13:22:58 +1200

    Options parsed in command runned

--- packages/cli/src/infra/commander/commands/run.command.ts
+++ packages/cli/src/infra/commander/commands/run.command.ts
@@ -1,58 +1,64 @@
-import { ILogger, MaybeUndefined } from "@ask-ell/core";
-import { existsSync } from "node:fs";
-import { readFile } from "node:fs/promises";
+import { ILogger, MaybeUndefined } from '@ask-ell/core';
+import { existsSync } from 'node:fs';
+import { readFile } from 'node:fs/promises';
 
-import { IProjectDTO } from "@ask/back-end-api";
+import { IProjectDTO } from '@ask/back-end-api';
 
-import { IGetProjectTasksUseCase, RunnableTask } from "../../../application";
-import { ActionCallbackParams, ChildCommand } from "./child.command";
-import { PROJECT_SETTINGS_FILE_PATH } from "../../../shared/path";
-import { runTask } from "../../../shared/task";
-import { GetProjectTasksUseCaseFactory } from "../../../shared/factories";
-
+import { IGetProjectTasksUseCase, RunnableTask } from '../../../application';
+import { ActionCallbackParams, ChildCommand } from './child.command';
+import { PROJECT_SETTINGS_FILE_PATH } from '../../../shared/path';
+import { runTask } from '../../../shared/task';
+import { GetProjectTasksUseCaseFactory } from '../../../shared/factories';
 
 export class RunCommand extends ChildCommand {
-    private getProjectTasksUseCase: MaybeUndefined<IGetProjectTasksUseCase>;
+  private getProjectTasksUseCase: MaybeUndefined<IGetProjectTasksUseCase>;
 
-    constructor(
-        logger: ILogger,
-        private getProjectTasksUseCaseFactory: GetProjectTasksUseCaseFactory,
-    ) {
-        super({
-            id: 'run',
-            logger
-        });
-        this
-            .description('run a project task')
-            .argument('<task>', 'task identifier')
-            .wrappedAction(this.run.bind(this));
+  constructor(
+    logger: ILogger,
+    private getProjectTasksUseCaseFactory: GetProjectTasksUseCaseFactory,
+  ) {
+    super({
+      id: 'run',
+      logger,
+    });
+    this.description('run a project task')
+      .argument('<task>', 'task identifier')
+      .wrappedAction(this.run.bind(this));
+  }
+
+  private async run({ options, args }: ActionCallbackParams): Promise<void> {
+    if (!this.getProjectTasksUseCase) {
+      this.getProjectTasksUseCase = this.getProjectTasksUseCaseFactory({
+        options,
+        logger: this.logger,
+      });
     }
 
-    private async run({ options, args }: ActionCallbackParams): Promise<void> {
-        if(!this.getProjectTasksUseCase){
-            this.getProjectTasksUseCase = this.getProjectTasksUseCaseFactory({
-                options,
-                logger: this.logger
-            });
-        }
-
-        if(!existsSync(PROJECT_SETTINGS_FILE_PATH)) {
-            throw new Error(`Project settings file not found at path: ${PROJECT_SETTINGS_FILE_PATH}`);
-        }
-
-        const project: IProjectDTO = JSON.parse(
-            await readFile(PROJECT_SETTINGS_FILE_PATH, 'utf-8')
-        );
-
-        const tasks: RunnableTask[] = await this.getProjectTasksUseCase.run(project);
-
-        const taskId: string = args[0];
-        const taskToRun: MaybeUndefined<RunnableTask> = tasks.find((task: RunnableTask): boolean => task.id === taskId);
-
-        if(!taskToRun) {
-            throw new Error(`Unknown task : "${taskId}"`);
-        }
-
-        runTask(this.logger)(taskToRun);
+    if (!existsSync(PROJECT_SETTINGS_FILE_PATH)) {
+      throw new Error(
+        `Project settings file not found at path: ${PROJECT_SETTINGS_FILE_PATH}`,
+      );
     }
+
+    const project: IProjectDTO = JSON.parse(
+      await readFile(PROJECT_SETTINGS_FILE_PATH, 'utf-8'),
+    );
+
+    const tasks: RunnableTask[] =
+      await this.getProjectTasksUseCase.run(project);
+
+    const taskId: string = args[0];
+    const taskToRun: MaybeUndefined<RunnableTask> = tasks.find(
+      (task: RunnableTask): boolean => task.id === taskId,
+    );
+
+    if (!taskToRun) {
+      throw new Error(`Unknown task : "${taskId}"`);
+    }
+
+    runTask({
+      logger: this.logger,
+      options,
+    })(taskToRun);
+  }
 }
--- packages/cli/src/shared/task.ts
+++ packages/cli/src/shared/task.ts
@@ -1,21 +1,34 @@
-import { ILogger } from "@ask-ell/core";
-import { execSync } from "node:child_process";
+import { ILogger } from '@ask-ell/core';
+import { execSync } from 'node:child_process';
 
 import { RunnableTask } from '../application';
+import { RootOptions } from './options';
 
+const runInstruction =
+  ({ logger, options }: { logger: ILogger; options: RootOptions }) =>
+  (instruction: string): void => {
+    if (instruction.includes('@ask')) {
+      let optionsFlagsStringified: string = '';
 
-const runInstruction = (logger: ILogger) => (instruction: string): void => {
-    if(instruction.includes('@ask')){
-        instruction = instruction.replace('@ask', process.argv[1]);
+      Object.keys(options).forEach((optionKey: string): void => {
+        optionsFlagsStringified += ` --${optionKey} ${options[optionKey as keyof RootOptions]}`;
+      });
+
+      instruction = instruction.replace(
+        '@ask',
+        process.argv[1] + optionsFlagsStringified,
+      );
     }
 
     logger.info(`Running instruction: ${instruction}`);
     execSync(instruction, { stdio: 'inherit' });
     logger.info(`Task completed !`);
-};
+  };
 
 export type TaskRunner = (task: RunnableTask) => void;
 
-export const runTask = (logger: ILogger): TaskRunner => (task: RunnableTask): void => {
-    task.instructions.forEach(runInstruction(logger));
-};
+export const runTask =
+  (params: { logger: ILogger; options: RootOptions }): TaskRunner =>
+  (task: RunnableTask): void => {
+    task.instructions.forEach(runInstruction(params));
+  };
